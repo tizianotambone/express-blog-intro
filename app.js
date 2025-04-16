20b3a4cc125d8dc12dcb9599480c91dc2fe9a84f
tizianotambone <tambonetiziano@gmail.com>
2025-04-16 13:47:04 +0200

    start ex

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+// console.log("hello world")
+const express = require('express');
+
+const app = express();
+const port = 3000;
+// inserisco al rotta base per gestire le richieste GET
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {})  
