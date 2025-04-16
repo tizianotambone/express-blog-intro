1c3ea0e2ab81bacf08ce4bedb51090f99aa92ac7
tizianotambone <tambonetiziano@gmail.com>
2025-04-16 15:10:31 +0200

    [UPD]try fix images

--- app.js
+++ app.js
@@ -16,31 +16,31 @@ app.get('/bacheca', (req, res) => {
             {
                 titolo: 'Ciambellone',
                 contenuto: 'dolce preferito',
-                immagine:"public/images/ciambellone.jpg",
+                immagine:"<img src='images/ciambellone.jpeg' >",
                 tags:["dolce","sfizioso"]
             }, 
             {
                 titolo: 'Cracker di barbabietola',
                 contenuto: 'croccante e delizioso',
-                immagine:"images/cracker_barbabietola.jpg",
+                immagine:"<img src='images/cracker_barbabietola.jpeg' >",
                 tags:["salato","croccante"]
             }, 
             {
                 titolo: 'Pane fritto dolce',
                 contenuto: 'fritto e accativante',
-                immagine:"iimages/pane_fritto_dolce.jpg",
+                immagine:"<img src='images/pane_dolce_fritto.jpeg' >",
                 tags:["dolce","sfizioso","ipercalorico"]
             }, 
             {
                 titolo: 'pasta alla barbabietola',
                 contenuto: 'carboidrati sfiziosi',
-                immagine:"images/pasta_barbabietola.jpg",
+                immagine:"<img src='images/pasta_barbabietola.jpeg' >",
                 tags:["salato","sfizioso","colorato"]
             }, 
             {
                 titolo: 'Torta Paesana',
                 contenuto: 'da nonna a nipote',
-                immagine:"images/torta_paesana.jpg",
+                immagine:"<img src='images/torta_paesana.jpeg'>",
                 tags:["dolce","sfizioso","tradizionae"]
             }]
             console.log(infoBlog)
