a5a5f18a8b9c78a85f708c26fc9cec492aab9be3
tizianotambone <tambonetiziano@gmail.com>
2025-04-16 14:13:43 +0200

    [UPD]creazione array

--- app.js
+++ app.js
@@ -3,10 +3,48 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
+app.use(express.static('public'));                    
 // inserisco al rotta base per gestire le richieste GET
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('server del mio blog');
 });
 
-app.listen(port, () => {})  
+app.get('/posts', (req, res) => {
+    const infoBlog = [
+            {
+                titolo: 'Ciambellone',
+                contenuto: 'dolce preferito',
+                immagine:"img./",
+                tags:["dolce","sfizioso"]
+            }, 
+            {
+                titolo: 'Cracker di barbabietola',
+                contenuto: 'croccante e delizioso',
+                immagine:"img./",
+                tags:["salato","croccante"]
+            }, 
+            {
+                titolo: 'Pane fritto dolce',
+                contenuto: 'fritto e accativante',
+                immagine:"img./",
+                tags:["dolce","sfizioso","ipercalorico"]
+            }, 
+            {
+                titolo: 'pasta alla barbabietola',
+                contenuto: 'carboidrati sfiziosi',
+                immagine:"img./",
+                tags:["salato","sfizioso","colorato"]
+            }, 
+            {
+                titolo: 'Torta Paesana',
+                contenuto: 'da nonna a nipote',
+                immagine:"img./",
+                tags:["dolce","sfizioso","tradizionae"]
+            }]
+
+            res.json(infoBlog)
+
+    })
+
+
