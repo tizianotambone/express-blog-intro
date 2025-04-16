8922d1cd9ff6a2535cf0975d30b23f41b394fb8f
tizianotambone <tambonetiziano@gmail.com>
2025-04-16 14:26:13 +0200

    [UPD]errore caricamento immagini

--- app.js
+++ app.js
@@ -15,31 +15,31 @@ app.get('/posts', (req, res) => {
             {
                 titolo: 'Ciambellone',
                 contenuto: 'dolce preferito',
-                immagine:"img./",
+                immagine:"public/images/ciambellone.jpg",
                 tags:["dolce","sfizioso"]
             }, 
             {
                 titolo: 'Cracker di barbabietola',
                 contenuto: 'croccante e delizioso',
-                immagine:"img./",
+                immagine:"public/images/cracker_barbabietola.jpg",
                 tags:["salato","croccante"]
             }, 
             {
                 titolo: 'Pane fritto dolce',
                 contenuto: 'fritto e accativante',
-                immagine:"img./",
+                immagine:"ipublic/images/pane_fritto_dolce.jpg",
                 tags:["dolce","sfizioso","ipercalorico"]
             }, 
             {
                 titolo: 'pasta alla barbabietola',
                 contenuto: 'carboidrati sfiziosi',
-                immagine:"img./",
+                immagine:"public/images/pasta_barbabietola.jpg",
                 tags:["salato","sfizioso","colorato"]
             }, 
             {
                 titolo: 'Torta Paesana',
                 contenuto: 'da nonna a nipote',
-                immagine:"img./",
+                immagine:"public/images/torta_paesana.jpg",
                 tags:["dolce","sfizioso","tradizionae"]
             }]
 
@@ -47,4 +47,10 @@ app.get('/posts', (req, res) => {
 
     })
 
+
+    // creo una rotta che restituisce un oggetto json con la lista dei post
+
+app.listen(port, () => {
+    console.log(`lista dei post:${port}`);
+  });
 
